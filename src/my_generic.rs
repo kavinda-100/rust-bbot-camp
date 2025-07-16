178dbe3d176e411a9ed60c90e1be27c9a63aac06
kavinda-100 <kavindarathnayake100@gmail.com>
2025-07-16 19:35:50 +0530

    Implement generic struct Point with methods and add .gitignore and project configuration files

--- src/my_generic.rs
+++ src/my_generic.rs
@@ -1,4 +1,26 @@
 pub fn start(){
-    println!("Starting my_generic module...");
-    // Additional initialization code can go here
+    // Generic structs in Rust
+    #[derive(Debug)]
+    struct Point<T> {
+        x: T,
+        y: T,
+    }
+    // Implementing methods for the generic struct
+    impl<T: std::fmt::Display> Point<T> {
+        fn new(x: T, y: T) -> Self {
+            Point { x, y }
+        }
+        fn display_info(&self) {
+            println!("Point({}, {})", self.x, self.y);
+        }
+    }
+
+    // Using the generic struct with different types
+    let int_point = Point::new(5, 10);
+    let float_point = Point::new(3.5, 7.2);
+    let string_point = Point::new("x", "y");
+    // Displaying the points
+    int_point.display_info();
+    float_point.display_info();
+    string_point.display_info();
 }
