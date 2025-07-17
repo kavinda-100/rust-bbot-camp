d5cea3aa5f40828d408fb3819945a5da802b17aa
kavinda-100 <kavindarathnayake100@gmail.com>
2025-07-17 12:40:22 +0530

    Enhance my_hash_map module with value removal, iteration, and length display

--- src/my_hash_map.rs
+++ src/my_hash_map.rs
@@ -3,14 +3,25 @@ use std::collections::HashMap;
 pub fn start(){
     
     let mut my_hash_map: HashMap<i32, String> = HashMap::new();
-    
+
+    // Inserting values into the HashMap
     my_hash_map.insert(1, String::from("One"));
     my_hash_map.insert(2, String::from("Two"));
     my_hash_map.insert(3, String::from("Three"));
+
+    // remove a value
+    // my_hash_map.remove(&2);
 
     let value = my_hash_map.get(&2);
     match value {
         Some(v) => println!("Value for key 2: {}", v),
         None => println!("Key not found"),
     }
+
+    // print the map using a for loop
+    for (key, value) in my_hash_map.iter() {
+        println!("Key: {}, Value: {}", key, value);
+    }
+    // Print the length of the HashMap
+    println!("HashMap length: {}", my_hash_map.len());
 }
