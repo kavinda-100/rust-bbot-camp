29df4f82e1ad98fc182d81d71e8341b12d5a9658
kavinda-100 <kavindarathnayake100@gmail.com>
2025-07-18 10:04:13 +0530

    Add naive date and leap year examples in my_chrono module

--- src/time_module/my_chrono.rs
+++ src/time_module/my_chrono.rs
@@ -17,4 +17,28 @@ pub fn start() {
         "Formatted date and time UTC: {}",
         utc_now.format("%Y-%m-%d %H:%M:%S")
     );
+
+    // Naive dateTime example using chrono
+    let naive_date = chrono::NaiveDate::from_ymd_opt(2023, 10, 1);
+    match naive_date {
+        Some(date) => {
+            println!("Naive date: {}", date);
+            // Formatting naive date to a string
+            println!("Formatted naive date: {}", date.format("%Y-%m-%d"));
+        }
+        None => {
+            println!("Invalid date");
+        }
+    }
+
+    // is leap year example using chrono
+    let is_leap_year = chrono::NaiveDate::from_yo_opt(2020, 366);
+    match is_leap_year {
+        Some(date) => {
+            println!("{} is a leap year", date);
+        }
+        None => {
+            println!("Invalid year");
+        }
+    }
 }
