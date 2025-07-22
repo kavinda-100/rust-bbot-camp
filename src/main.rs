59b55f03f4a39d4ac0040f9c40a10523cc355c90
kavinda-100 <kavindarathnayake100@gmail.com>
2025-07-22 19:12:53 +0530

    Comment out async main function and related calls for future reference

--- src/main.rs
+++ src/main.rs
@@ -46,77 +46,77 @@ mod tasks;
 mod time_module;
 mod user_inputs;
 
-// ================================ without asyncs ================================
-// fn main() {
-//     // Call the start function from the my_functions module
-//     // my_functions::start();
-//
-//     // Call the start function from the control_flow module
-//     // control_flow::start();
-//
-//     // Call the start function from the user_inputs module
-//     // user_inputs::start();
-//
-//     // Call the start function from the my_closures module
-//     // my_closures::start();
-//
-//     // Call the start function from the my_match_expression module
-//     // my_match_expression::start();
-//
-//     // Call the start function from the my_enums module
-//     // my_enums::start();
-//
-//     // Call the start function from the my_option module
-//     // my_option::start();
-//
-//     // Call the start function from the my_struct module
-//     // my_struct::start();
-//
-//     // Call the start function from my_methods module
-//     // my_methods::start()
-//
-//     // Call the start function from the my_generic module
-//     // my_generic::start();
-//
-//     // Call the start function from the my_traits module
-//     // my_traits::start();
-//
-//     // Call the start function from the my_vec module
-//     // my_vec::start();
-//
-//     // Call the start function from the my_hash_map
-//     // my_hash_map::start();
-//
-//     // Call the start function from the my_iterator module
-//     // my_iterator::start();
-//
-//     // Call the start function from the time_module module
-//     // time_module::my_time::start();
-//     // time_module::my_chrono::start();
-//
-//     // Call the start function from the file_system module
-//     // file_system::my_file_system::start();
-//
-//     // Call the start function from the my_args module
-//     // my_args::start();
-//
-//     // Call the start function from the clap module
-//     // clap::my_cli::start();
-//
-//     // Call the start function from the my_serde module
-//     // my_serde::start();
-//
-//     // Call the start function from the my_http module
-//     // my_http::start();
-//
-//     // Call the start function from the my_option_overload module
-//     my_option_overload::start();
-//
-//     // Call the start function from the tasks::sort_values module
-//     // tasks::sort_values::start();
-//
-//     println!("End of main function");
-// }
+// ================================ without async ================================
+fn main() {
+    // Call the start function from the my_functions module
+    // my_functions::start();
+
+    // Call the start function from the control_flow module
+    // control_flow::start();
+
+    // Call the start function from the user_inputs module
+    // user_inputs::start();
+
+    // Call the start function from the my_closures module
+    // my_closures::start();
+
+    // Call the start function from the my_match_expression module
+    // my_match_expression::start();
+
+    // Call the start function from the my_enums module
+    // my_enums::start();
+
+    // Call the start function from the my_option module
+    // my_option::start();
+
+    // Call the start function from the my_struct module
+    // my_struct::start();
+
+    // Call the start function from my_methods module
+    // my_methods::start()
+
+    // Call the start function from the my_generic module
+    // my_generic::start();
+
+    // Call the start function from the my_traits module
+    // my_traits::start();
+
+    // Call the start function from the my_vec module
+    // my_vec::start();
+
+    // Call the start function from the my_hash_map
+    // my_hash_map::start();
+
+    // Call the start function from the my_iterator module
+    // my_iterator::start();
+
+    // Call the start function from the time_module module
+    // time_module::my_time::start();
+    // time_module::my_chrono::start();
+
+    // Call the start function from the file_system module
+    // file_system::my_file_system::start();
+
+    // Call the start function from the my_args module
+    // my_args::start();
+
+    // Call the start function from the clap module
+    // clap::my_cli::start();
+
+    // Call the start function from the my_serde module
+    // my_serde::start();
+
+    // Call the start function from the my_http module
+    // my_http::start();
+
+    // Call the start function from the my_option_overload module
+    my_option_overload::start();
+
+    // Call the start function from the tasks::sort_values module
+    // tasks::sort_values::start();
+
+    println!("End of main function");
+}
 
 // ================================ with async ================================
 
@@ -127,8 +127,8 @@ mod user_inputs;
 // }
 
 // ===== with Tokio =====
-#[tokio::main]
-async fn main() {
-    // Call the start function from the my_tokio module
-    asyncs::my_tokio::start().await;
-}
+// #[tokio::main]
+// async fn main() {
+//     // Call the start function from the my_tokio module
+//     asyncs::my_tokio::start().await;
+// }
