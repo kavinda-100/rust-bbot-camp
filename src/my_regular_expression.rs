66c2c81aea1461a7f3ba9783e7a70fe0fe8fec7d
kavinda-100 <kavindarathnayake100@gmail.com>
2025-07-23 19:58:41 +0530

    Add regex group matching function to handle complex patterns

--- src/my_regular_expression.rs
+++ src/my_regular_expression.rs
@@ -14,6 +14,45 @@ pub fn start() {
     let pattern = "[A-Z]{1}[a-z]{2,8}"; // Pattern to match a string that starts with an uppercase letter followed by 2 to 8 lowercase letters.
     let input = "Kavinda is going to the market";
     find_regex(pattern, input);
+
+    println!("=====================");
+    let pattern = r"([A-Z]{1}[a-z]{2,8})|([A-Z]{1}[a-z]{2,8})|([0-9]{4})"; // Pattern to match a string that starts with an uppercase letter followed by 2 to 8 lowercase letters, then a pipe, then another uppercase letter followed by 2 to 8 lowercase letters, then another pipe, and finally a 4-digit number.
+    let input = "Kavinda|Rathnayake|2025";
+    find_regex_group(pattern, input);
+}
+
+fn find_regex_group(patten: &str, input: &str) {
+    // Compile the regular expression
+    let regex_pattern = Regex::new(patten);
+    // Check if the regex was compiled successfully
+    match &regex_pattern {
+        Ok(re) => {
+            println!("re: {:?}", re);
+        }
+        Err(e) => {
+            println!("Error compiling regex: {}", e);
+            return;
+        }
+    }
+    // Check if the input matches the pattern
+    let res = regex_pattern.unwrap().captures(input);
+    // Print the result
+    match res {
+        Some(captures) => {
+            println!(
+                "The input '{:?}' - '{:?}' - '{:?}'",
+                captures.get(0).map_or("", |m| m.as_str()),
+                captures.get(1).map_or("", |m| m.as_str()),
+                captures.get(2).map_or("", |m| m.as_str())
+            );
+        }
+        None => {
+            println!(
+                "The input '{}' does not contain a match for the pattern '{}'",
+                input, patten
+            );
+        }
+    }
 }
 
 fn find_regex(patten: &str, input: &str) {
