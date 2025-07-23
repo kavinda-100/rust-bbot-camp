8970e5484c904f46dba1a0e7053985329aa98777
kavinda-100 <kavindarathnayake100@gmail.com>
2025-07-23 19:43:15 +0530

    Refactor regex testing functions to improve pattern matching and add input handling

--- src/my_regular_expression.rs
+++ src/my_regular_expression.rs
@@ -5,16 +5,54 @@ pub fn start() {
     println!("Testing regular expressions...");
 
     // Call the test_regex function to perform the regex test
-    test_regex();
+    // Define a regular expression pattern
+    let pattern = "[A-Z]{1}[a-z]{2,8}"; // Pattern to match a string that starts with an uppercase letter followed by 2 to 8 lowercase letters.
+    let input = "Kavinda";
+    test_regex(pattern, input);
+
+    println!("=====================");
+    let pattern = "[A-Z]{1}[a-z]{2,8}"; // Pattern to match a string that starts with an uppercase letter followed by 2 to 8 lowercase letters.
+    let input = "Kavinda is going to the market";
+    find_regex(pattern, input);
 }
 
-fn test_regex() {
-    // Define a regular expression pattern
-    let pattern = "[A-Z]{1}[a-z]{2,8}";
-    let input = "Kavinda";
-
+fn find_regex(patten: &str, input: &str) {
     // Compile the regular expression
-    let regex_pattern = Regex::new(pattern);
+    let regex_pattern = Regex::new(patten);
+    // Check if the regex was compiled successfully
+    match &regex_pattern {
+        Ok(re) => {
+            println!("re: {:?}", re);
+        }
+        Err(e) => {
+            println!("Error compiling regex: {}", e);
+            return;
+        }
+    }
+    // Check if the input matches the pattern
+    let res = regex_pattern.unwrap().find(input);
+    // Print the result
+    match res {
+        Some(matched) => {
+            println!(
+                "The input '{}' contains a match for the pattern '{}': '{}'",
+                input,
+                patten,
+                matched.as_str()
+            );
+        }
+        None => {
+            println!(
+                "The input '{}' does not contain a match for the pattern '{}'",
+                input, patten
+            );
+        }
+    }
+}
+
+fn test_regex(patten: &str, input: &str) {
+    // Compile the regular expression
+    let regex_pattern = Regex::new(patten);
     // Check if the regex was compiled successfully
     match &regex_pattern {
         Ok(re) => {
@@ -29,11 +67,11 @@ fn test_regex() {
     let res = regex_pattern.unwrap().is_match(input);
     // Print the result
     if res {
-        println!("The input '{}' matches the pattern '{}'", input, pattern);
+        println!("The input '{}' matches the pattern '{}'", input, patten);
     } else {
         println!(
             "The input '{}' does not match the pattern '{}'",
-            input, pattern
+            input, patten
         );
     }
 }
