7964b7b99521db74fb741e57cb2e95dcbc42b923
kavinda-100 <kavindarathnayake100@gmail.com>
2025-07-24 19:01:17 +0530

    Add validation to get_full_name function and corresponding tests

--- src/unit_tests/my_code.rs
+++ src/unit_tests/my_code.rs
@@ -10,6 +10,14 @@ pub mod my_code {
         println!("Full name: {}", full_name);
     }
     pub fn get_full_name(first_name: &str, last_name: &str) -> String {
+        // Check if first name or last name is empty
+        if first_name.is_empty() || last_name.is_empty() {
+            panic!("First name and last name cannot be empty");
+        }
+        else if first_name.contains(&['*', '%', '$', '#', '`', '^', '+']) || last_name.contains(&['*', '%', '$', '#', '`', '^', '+']) {
+            panic!("First name or last name contains special characters");
+        }
+        // Return the full name by concatenating first and last names
         format!("{} {}", first_name, last_name)
     }
 }
--- src/unit_tests/my_test.rs
+++ src/unit_tests/my_test.rs
@@ -7,6 +7,22 @@ mod test {
         let first_name = "John";
         let last_name = "Doe";
         let full_name = get_full_name(first_name, last_name);
-        assert_eq!(full_name, "John Doe");
+        assert_eq!(full_name, format!("{} {}", first_name, last_name));
+    }
+
+    #[test]
+    #[should_panic(expected = "First name or last name contains special characters")]
+    fn test_get_full_name_special_chars() {
+        let first_name = "*John";
+        let last_name = "Doe";
+        _ = get_full_name(first_name, last_name);
+    }
+
+    #[test]
+    #[should_panic(expected = "First name and last name cannot be empty")]
+    fn test_get_full_name_empty() {
+        let first_name = "";
+        let last_name = "Doe";
+        _ = get_full_name(first_name, last_name);
     }
 }
