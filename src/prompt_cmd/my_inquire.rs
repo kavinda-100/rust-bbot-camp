8eac392c2b09701dd1246b665076aa6030ed3498
kavinda-100 <kavindarathnayake100@gmail.com>
2025-07-24 20:33:02 +0530

    Add player age validation and prompt in my_inquire module

--- src/prompt_cmd/my_inquire.rs
+++ src/prompt_cmd/my_inquire.rs
@@ -1,10 +1,15 @@
 use colorize::AnsiColor;
 use inquire::CustomUserError;
+use inquire::error::InquireResult;
 use inquire::validator::Validation;
 
 /// This is example of use of inquire crate
 pub fn start() {
-    // ========================= Validate inputs with inquire =========================
+    // prompt user for inputs using inquire crate
+    let mut prompt_results: Vec<String> = Vec::new();
+
+    //? ========================= Validate inputs with inquire =========================
+    // validate player name
     let player_name_validation = |input: &str| -> Result<Validation, CustomUserError> {
         return if input.is_empty() {
             Ok(Validation::Invalid("Player name cannot be empty".into()))
@@ -20,11 +25,19 @@ pub fn start() {
             Ok(Validation::Valid)
         };
     };
+    // validate player age
+    let player_age_validation = |input: &str| -> Result<Validation, CustomUserError> {
+        return if input.is_empty() {
+            Ok(Validation::Invalid("Player age cannot be empty".into()))
+        } else if input.parse::<u32>().is_err() {
+            Ok(Validation::Invalid("Player age must be a number".into()))
+        } else {
+            Ok(Validation::Valid)
+        };
+    };
 
     // ========================= Prompt with inquire =========================
     println!("{0}", "============================".green());
-    // println!("{0}", "Are you ready to start?".green());
-    let mut prompt_results: Vec<String> = Vec::new();
 
     let proceed = inquire::prompt_confirmation("Are you ready to proceed? (y/n)");
     match proceed {
@@ -49,6 +62,21 @@ pub fn start() {
     match player_name {
         Ok(name) => {
             prompt_results.push(name);
+        }
+        Err(e) => {
+            println!("{0}", format!("Error: {}", e).red());
+            return;
+        }
+    }
+    // ask player's age
+    let player_age = inquire::Text::new("Please enter your player age (number):")
+        .with_validator(player_age_validation)
+        .with_help_message("This will be your player age in the game.")
+        .with_initial_value("18")
+        .prompt();
+    match player_age {
+        Ok(age) => {
+            prompt_results.push(age);
         }
         Err(e) => {
             println!("{0}", format!("Error: {}", e).red());
